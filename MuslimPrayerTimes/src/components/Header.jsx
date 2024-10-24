dfc46afb64f6c726d0a80cdf537adb123186616f
Subuhan BCA <121153374+SubuhanLathif@users.noreply.github.com>
2024-10-24 12:15:27 +0530

    Add files via upload

--- MuslimPrayerTimes/src/components/Header.jsx
+++ MuslimPrayerTimes/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import SiteLogo from '../assets/SiteLogo.png'
 export const Header = () => {
   return (
-    < div className="px-lg-5 px-md-4 px-3">
+    < div className="px-lg-5 px-md-4 px-3 sticky-top bg-white">
     <div className="header d-flex container py-2 px-0 justify-content-between align-items-center">
         <div className="logo"><img src={SiteLogo} alt="site-logo"/></div>
-        <div className="header-btn"><button className='btn btn-sm px-4 py-2 rounded-pill'>Contact</button></div>
+        <div className="header-btn">
+          <a href="https://subuhanbca.netlify.app/contact" target="_blank" rel="noopener noreferrer">
+          <button className='btn btn-sm px-4 py-2 rounded-pill'>Contact</button>
+          </a>
+        </div>
     </div>
     </div>
   )
